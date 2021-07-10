90256f33dec225ac0858db7910b3200523b7dd76
revsic <revsic99@gmail.com>
2021-07-10 16:42:19 +0900

    reader: Add data reader interface

---
+++ datasets/reader.py
@@ -0,0 +1,23 @@
+from typing import Callable
+
+import tensorflow as tf
+
+
+class DataReader:
+    """Interface of the data reader for efficient train-test split.
+    """
+    def dataset(self) -> tf.data.Dataset:
+        """Return file reader.
+        Returns:
+            file-format datum reader, without any preprocessor for fast train-text split.
+        """
+        raise NotImplementedError('DataReader.rawset is not implemented')
+    
+    def preproc(self) -> Callable:
+        """Return data preprocessor.
+        Returns:
+            preprocessor, required format,
+                text: tf.string, text.
+                speech: [tf.float32; T], speech signal in range (-1, 1).
+        """
+        raise NotImplementedError('DataReader.preproc is not implemented')
