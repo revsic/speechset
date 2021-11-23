ed94d69d8021eea6f379afad325beb84e235951b
revsic <revsic99@gmail.com>
2021-11-24 00:53:50 +0900

    sample: Fix samples

--- sample.py
+++ sample.py
@@ -6,7 +6,7 @@ sys.path.pop()
 
 
 # construct data reader
-lj = speechset.datasets.LJSpeech()
+lj = speechset.datasets.LJSpeech('D:\\dataset\\LJSpeech-1.1')
 
 # construct configuration
 config = speechset.Config()
@@ -14,22 +14,22 @@ config = speechset.Config()
 # construct acoustic model
 acoustic = speechset.AcousticDataset(lj, config)
 
-# unpack
-text, mel, textlen, mellen = acoustic[0]
-print(text.shape, mel.shape, textlen.shape, mellen.shape)
+# indexing
+text, mel = acoustic[0]
+print(text.shape, mel.shape)
 
 # split sample
 testset = acoustic.split(1000)
-# unpack
-text, mel, textlen, mellen = acoustic.collate(acoustic[0:3])
+# back pack
+text, mel, textlen, mellen = acoustic[0:3]
 print(text.shape, mel.shape, textlen.shape, mellen.shape)
-
-text, mel, textlen, mellen = next(iter(testset))
-print(text.shape, mel.shape, textlen.shape, mellen.shape)
+# iteration
+text, mel = next(iter(testset))
+print(text.shape, mel.shape)
 
 # construct vocoder model
 vocoder = speechset.VocoderDataset(lj, config)
 
 # unpack
-mel, audio, mellen, audiolen = next(iter(vocoder))
+mel, audio, mellen, audiolen = vocoder[0:3]
 print(mel.shape, audio.shape, mellen.shape, audiolen.shape)
