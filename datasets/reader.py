b18caa232f014a0f87ebff170f50edeb1c6ce808
revsic <revsic99@gmail.com>
2022-12-08 22:15:25 +0900

    reader: Add count-speakers

--- datasets/reader.py
+++ datasets/reader.py
@@ -19,3 +19,10 @@ class DataReader:
                 speech: [np.float32; T], speech signal in range (-1, 1).
         """
         raise NotImplementedError('DataReader.preproc is not implemented')
+
+    def count_speakers(self) -> int:
+        """Count the number of speakers.
+        Returns:
+            the number of the speakers.
+        """
+        raise NotImplementedError('DataReader.count_speakers is not implemented')
